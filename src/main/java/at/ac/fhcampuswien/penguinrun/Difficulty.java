ec11b234a4fc6bb163396b2c463865ff1326e625
itron1x <144708200+itron1x@users.noreply.github.com>
2023-12-27 18:25:30 +0100

    Added working UI for main menu.

--- src/main/java/at/ac/fhcampuswien/penguinrun/Difficulty.java
+++ src/main/java/at/ac/fhcampuswien/penguinrun/Difficulty.java
@@ -1,4 +1,26 @@
 package at.ac.fhcampuswien.penguinrun;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Difficulty {
+    @FXML
+    private Button backBtn;
+
+    public void onBack() throws IOException {
+        Stage stage = (Stage) backBtn.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("start-menu.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, 1270,720);
+        scene.getStylesheets().add(getClass().getResource("/at/ac/fhcampuswien/penguinrun/style.css").toExternalForm());
+        stage.setTitle("PenguinRun");
+        stage.setScene(scene);
+    }
 }
--- src/main/java/at/ac/fhcampuswien/penguinrun/Main.java
+++ src/main/java/at/ac/fhcampuswien/penguinrun/Main.java
@@ -3,16 +3,21 @@ package at.ac.fhcampuswien.penguinrun;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("start-menu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        Image icon = new Image("/img/pgnBig.png");
+        scene.getStylesheets().add(getClass().getResource("/at/ac/fhcampuswien/penguinrun/style.css").toExternalForm());
         stage.setTitle("PenguinRun");
+        stage.getIcons().add(icon);
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
