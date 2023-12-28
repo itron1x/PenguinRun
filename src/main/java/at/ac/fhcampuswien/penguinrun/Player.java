9f01cf9c793363d0e91f59e57c00b951b365c4b4
plukesch <patrick.lukesch@chello.at>
2023-12-28 13:44:04 +0100

    Included character images.

--- src/main/java/at/ac/fhcampuswien/penguinrun/Player.java
+++ src/main/java/at/ac/fhcampuswien/penguinrun/Player.java
@@ -3,6 +3,7 @@ package at.ac.fhcampuswien.penguinrun;
 import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 
@@ -18,32 +19,41 @@ public class Player implements Initializable {
     private boolean downPressed = false; //S + DOWN
     private boolean leftPressed = false; //A + LEFT
     private boolean rightPressed = false; //D + RIGHT
+    private final int speed = 1; //Movement Speed Penguin
+
+    Image pgnStill = new Image("img/pgnStill.png",true);
+    Image pgnAnim = new Image("img/pgnAnim.gif",true);
     
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        setupKeyHandlers();
+        continuousMovement();
     }
 
     public void handleKeyPressed(KeyEvent event) {
         switch (event.getCode()) {
             //WASD + ARROW KEYS
             case W, UP:
+                pgn.setRotate(0);
                 upPressed = true;
                 break;
             case A, LEFT:
+                pgn.setRotate(-90);
                 leftPressed = true;
                 break;
             case S, DOWN:
+                pgn.setRotate(180);
                 downPressed = true;
                 break;
             case D, RIGHT:
+                pgn.setRotate(90);
                 rightPressed = true;
                 break;
         }
     }
 
-    public void handleKeyReleased(KeyEvent event) {
+    public void keyReleased(KeyEvent event) {
+        pgn.setImage(pgnStill);
         switch (event.getCode()) {
             //WASD + ARROW KEYS
             case W, UP:
