bac606d3ff6aa5dd62557ddb0e01f0301d41ecb8
Huawei <alfonso.de.vera@stud.fh-campuswien.ac.at>
2024-01-21 21:33:01 +0100

    Added comments to Countdown.java methods. Removed unused method in Countdown.java

--- src/main/java/at/ac/fhcampuswien/penguinrun/game/Countdown.java
+++ src/main/java/at/ac/fhcampuswien/penguinrun/game/Countdown.java
@@ -16,12 +16,20 @@ public class Countdown {
     private boolean paused;
     private static int TimeLeft;
 
+    /**
+     * Creates a timer that acts as the countdown for the game. This will be called in the GameManager class.
+     * @param initialSeconds
+     */
     public Countdown(int initialSeconds) {
         this.secondsRemaining = initialSeconds;
         initializeTimeline();
         this.paused = false;
     }
 
+    /**
+     * Creates and starts the timer letting the time tick down if the game isn't paused. If the time reaches zero the timer stops, ensuring that
+     * the timer doesn't display negative numbers.
+     */
     private void initializeTimeline() {
         timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
             if (!paused) {
@@ -36,23 +44,33 @@ public class Countdown {
         timeline.setCycleCount(Timeline.INDEFINITE);
     }
 
+    /**
+     * Method that starts the timer.
+     */
     public void start() {
         timeline.play();
     }
 
+    /**
+     * Turns the boolean "paused" to true to let the timer know if the game is paused or not. If "paused" is true,
+     * then the timer stops ticking.
+     */
     public void pause() {
         paused = true;
     }
 
+    /**
+     * Turns the boolean "paused" to false. Lets the timer know that the player has unpaused the game and resumed
+     * playing, letting the timer continue to tick down from where it left off.
+     */
     public void resume() {
         paused = false;
     }
 
-    public void reset(int initialSeconds) {
-        this.secondsRemaining = initialSeconds;
-        initializeTimeline();
-    }
-
+    /**
+     * A getter method for secondsRemaining.
+     * @return
+     */
     public int getSecondsRemaining() {
         return secondsRemaining;
     }
